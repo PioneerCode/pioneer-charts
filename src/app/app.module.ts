6fc129039159809f4b503a836db47849656a0142
chad-ramos <jedi_obi_wan_@hotmail.com>
2018-03-09 16:33:13 -0600

    Scaffold table and line area chart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BarChartComponent } from './charts/bar-chart/bar-chart.component';
+import { TableComponent } from './charts/table/table.component';
+import { LineAreaChartComponent } from './charts/line-area-chart/line-area-chart.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    BarChartComponent
+    BarChartComponent,
+    TableComponent,
+    LineAreaChartComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/charts/line-area-chart/line-area-chart.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pc-line-area-chart',
+  templateUrl: './line-area-chart.component.html',
+  styleUrls: ['./line-area-chart.component.scss']
+})
+export class LineAreaChartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

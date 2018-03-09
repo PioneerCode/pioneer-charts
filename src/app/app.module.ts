779488504210da5e3d7d047d175d76091f777738
chad-ramos <jedi_obi_wan_@hotmail.com>
2018-03-09 15:36:15 -0600

    Add first chart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { BarChartComponent } from './charts/bar-chart/bar-chart.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BarChartComponent
   ],
   imports: [
     BrowserModule

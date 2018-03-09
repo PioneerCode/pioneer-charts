7571a315a6bd162b47c1ad8c21e1609af2ec0c35
chad-ramos <jedi_obi_wan_@hotmail.com>
2018-03-09 16:28:49 -0600

    Add base styling

--- src/app/charts/bar-chart/bar-chart.component.ts
+++ src/app/charts/bar-chart/bar-chart.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-bar-chart',
+  selector: 'pc-bar-chart',
   templateUrl: './bar-chart.component.html',
   styleUrls: ['./bar-chart.component.scss']
 })
 export class BarChartComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit() {
   }
-
 }

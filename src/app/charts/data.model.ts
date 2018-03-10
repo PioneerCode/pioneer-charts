f705dcf23b35feced4de1720df30a0629b7e462e
chad-ramos <jedi_obi_wan_@hotmail.com>
2018-03-10 09:24:17 -0600

    Add data model

---
+++ src/app/charts/data.model.ts
@@ -0,0 +1,5 @@
+export interface IData {
+  key: string | number;
+  value: string | number;
+  data: IData[];
+}
---
+++ src/app/charts/table/table.model.ts
@@ -0,0 +1,5 @@
+import { IData } from '../data.model';
+
+export interface ITableConfig {
+  data: IData[];
+}

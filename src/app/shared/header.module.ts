f4350606faab5b1530e9d61bec48f7201a774383
chad-ramos <jedi_obi_wan_@hotmail.com>
2018-03-10 14:07:56 -0600

    Add shared module

---
+++ src/app/shared/header.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+    HeaderComponent
+  ]
+})
+export class ChartModule { }

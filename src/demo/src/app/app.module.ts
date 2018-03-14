52b26b78fbe4f5e6a4b970f6efe6c49ce3456f7f
chad-ramos <jedi_obi_wan_@hotmail.com>
2018-03-14 10:28:16 -0500

    Remove deleted

--- src/demo/src/app/app.module.ts
+++ src/demo/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-import { PioneerCodeAngularChartModule } from './pcac/pcac.module';
 import { PcacRepository } from './repository/pcac.repository';
 
 @NgModule({

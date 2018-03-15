9f725f8c4a77566fe4b716108e6f45250a326b9f
chad-ramos <jedi_obi_wan_@hotmail.com>
2018-03-15 11:49:23 -0500

    IDE fix

--- src/dev/src/pcac/public-api.ts
+++ src/dev/src/pcac/public-api.ts
@@ -1,4 +1,4 @@
-// export * from '@pioneer-code/pioneer-code-angular-charts/header';
-// export * from '@pioneer-code/pioneer-code-angular-charts/table';
-// export * from '@pioneer-code/pioneer-code-angular-charts/bar-chart';
-// export * from '@pioneer-code/pioneer-code-angular-charts/line-area-chart';
+export * from '@pioneer-code/pioneer-code-angular-charts/header';
+export * from '@pioneer-code/pioneer-code-angular-charts/table';
+export * from '@pioneer-code/pioneer-code-angular-charts/bar-chart';
+export * from '@pioneer-code/pioneer-code-angular-charts/line-area-chart';

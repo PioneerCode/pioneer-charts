2dfeb0a55f0d80abc6dc5b830e9601dc548d32bd
chad-ramos <jedi_obi_wan_@hotmail.com>
2018-03-29 17:29:17 -0500

    Classify comments

--- src/apps/demo/src/app/app.module.ts
+++ src/apps/demo/src/app/app.module.ts
@@ -1,8 +1,42 @@
+/**
+ * Angular
+ */
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
+import { AppComponent } from './app.component';
+
+/**
+ * Demo deps
+ */
 import { PcacRepository } from './repository/pc.repository';
+import { PcService } from './services/pc.service';
+
+/**
+ * Demo Pages
+ */
+import { HomeComponent } from './pages/home/home.component';
+import { LineAreaChartComponent } from './pages/line-area-chart/line-area-chart.component';
+import { BarChartComponent } from './pages/bar-chart/bar-chart.component';
+import { HeaderComponent } from './pages/header/header.component';
+import { TableComponent } from './pages/table/table.component';
+import { GetStartedComponent } from './pages/get-started/get-started.component';
+import { DemoComponent } from './pages/demo/demo.component';
+import { PieChartComponent } from './pages/pie-chart/pie-chart.component';
+
+/**
+ * Demo Components
+ */
+import { PageHeaderComponent } from './components/page-header/page-header.component';
+import { PrismComponent } from './components/prism/prism.component';
+import { ChartNavComponent } from './components/chart-nav/chart-nav.component';
+import { NavComponent } from './components/nav/nav.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+/**
+ * Pioneer Charts
+ */
 import {
   PcacCoreModule,
   PcacHeaderModule,
@@ -12,21 +46,7 @@ import {
   PcacLineAreaChartModule,
   PcacPieChartModule
 } from '@pioneer-code/pioneer-charts';
-import { HomeComponent } from './pages/home/home.component';
-import { LineAreaChartComponent } from './pages/line-area-chart/line-area-chart.component';
-import { BarChartComponent } from './pages/bar-chart/bar-chart.component';
-import { HeaderComponent } from './pages/header/header.component';
-import { TableComponent } from './pages/table/table.component';
-import { RouterModule, Routes } from '@angular/router';
-import { PcService } from './services/pc.service';
-import { GetStartedComponent } from './pages/get-started/get-started.component';
-import { PageHeaderComponent } from './components/page-header/page-header.component';
-import { PrismComponent } from './components/prism/prism.component';
-import { ChartNavComponent } from './components/chart-nav/chart-nav.component';
-import { NavComponent } from './components/nav/nav.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { DemoComponent } from './pages/demo/demo.component';
-import { PieChartComponent } from './pages/pie-chart/pie-chart.component';
+
 
 const appRoutes: Routes = [
   { path: 'guides/get-started', component: GetStartedComponent },

d3b94fc5a195627df7013f53306c24b4d2b9cbb3
chad-ramos <jedi_obi_wan_@hotmail.com>
2018-04-16 12:32:16 -0500

    Add theme build via node-sass

--- src/build/sass.js
+++ src/build/sass.js
@@ -2,7 +2,8 @@
  * Move sass assets to lib dist
  */
 const copyfiles = require('copyfiles');
+const chalk = require('chalk');
 
 copyfiles(['./libs/pcac/**/*.scss', './dist/bundles/@pioneer-code/pioneer-charts/scss'], { up: 2 }, function () {
-  console.log('Sass moved.');
+  console.log(chalk.cyan('Pioneer Charts: Sass moved!'));
 });

3e5fd098d003cff59cca6907a9413c9c1ba07ec9
chad-ramos <jedi_obi_wan_@hotmail.com>
2018-05-03 09:42:30 -0500

    Abstrac axis build pipeline

--- src/libs/pcac/core/axis.builder.ts
+++ src/libs/pcac/core/axis.builder.ts
@@ -1,5 +1,6 @@
 import { axisBottom, axisLeft } from 'd3-axis';
 import { selection, BaseType } from 'd3-selection';
+import { format } from 'd3-format';
 
 export interface IPcacAxisBuilderConfig {
   svg: d3.Selection<BaseType, {}, HTMLElement, any>;
@@ -7,17 +8,37 @@ export interface IPcacAxisBuilderConfig {
   xScale: any;
   yScale: any;
   numberOfTicks: number;
+  yFormat?: string;
+  xFormat?: string;
 }
 
 export class PcacAxisBuilder {
   drawAxis(config: IPcacAxisBuilderConfig): void {
+    this.drawXAxis(config);
+    this.drawYAxis(config);
+  }
+
+  private drawYAxis(config: IPcacAxisBuilderConfig) {
+
+        // switch (this.config.tickFormat.toLocaleLowerCase()) {
+    //   case 'percentage':
+    //     axisY.tickFormat(d3.format('.0%'));
+    //     break;
+    //   case 'minutes':
+    //     axisY.tickFormat((d) => d + 'm');
+    //     break;
+    // }
+    const yAxis = axisLeft(config.yScale).ticks(config.numberOfTicks);
+    config.svg.append('g')
+      .attr('class', 'pcac-y-axis')
+      .call(yAxis);
+  }
+
+  private drawXAxis(config: IPcacAxisBuilderConfig) {
+    const xAxis = axisBottom(config.xScale).ticks(config.numberOfTicks);
     config.svg.append('g')
       .attr('class', 'pcac-x-axis')
       .attr('transform', 'translate(0,' + config.height + ')')
-      .call(axisBottom(config.xScale).ticks(config.numberOfTicks));
-
-    config.svg.append('g')
-      .attr('class', 'pcac-y-axis')
-      .call(axisLeft(config.yScale).ticks(config.numberOfTicks));
+      .call(xAxis);
   }
 }

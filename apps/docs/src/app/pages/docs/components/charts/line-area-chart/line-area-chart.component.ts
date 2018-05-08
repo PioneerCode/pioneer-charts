2f1596dfdc419efbf0eef8a368c52319983ab6cb
chad-ramos <jedi_obi_wan_@hotmail.com>
2018-05-08 11:50:50 -0500

    Add click events to docs

--- apps/docs/src/app/pages/docs/components/charts/line-area-chart/line-area-chart.component.ts
+++ apps/docs/src/app/pages/docs/components/charts/line-area-chart/line-area-chart.component.ts
@@ -37,6 +37,6 @@ export class LineAreaChartComponent {
   ] as IJumpNav[];
   markupCode = `<pcac-line-area-chart [config]="config"></pcac-line-area-chart>`;
   importCode = `import { PcacLineAreaChartModule } from '@pioneer-code/pioneer-charts';`;
-  constructor(public pcacService: PcService) { }
+  constructor(public pcService: PcService) { }
 }
 

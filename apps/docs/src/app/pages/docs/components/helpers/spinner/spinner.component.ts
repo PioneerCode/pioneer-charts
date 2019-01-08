1c86b9736ab03f9061b34387ba5704d0b047afc0
chad-ramos <jedi_obi_wan_@hotmail.com>
2019-01-08 11:00:38 -0600

    Bump version

--- apps/docs/src/app/pages/docs/components/helpers/spinner/spinner.component.ts
+++ apps/docs/src/app/pages/docs/components/helpers/spinner/spinner.component.ts
@@ -1,16 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { IJumpNav, JumpNavLevel } from 'apps/docs/src/app/components/jump-nav/jump-nav.component';
 
 @Component({
   selector: 'pc-spinner',
   templateUrl: './spinner.component.html',
   styleUrls: ['./spinner.component.scss']
 })
-export class SpinnerComponent implements OnInit {
+export class SpinnerComponent {
   public importCode = `import { PcacSpinnerModule } from '@pioneer-code/pioneer-charts';`;
   public markupCode = `<pcac-spinner [showSpinner]="false" [showOverlay]="false"></pcac-spinner>`;
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  public jumpNav = [
+    {
+      key: 'Spinner',
+      value: 'spinner',
+      level: JumpNavLevel.h1
+    }
+  ] as IJumpNav[];
 }
--- apps/docs/src/environments/environment.prod.ts
+++ apps/docs/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  version: '0.17.x'
+  version: '0.18.x'
 };

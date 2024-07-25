2de609b59a4cbe027d3b9e27b71935d45b77c832
chad-ramos <chad@pioneercode.com>
2024-07-25 11:01:02 -0500

    add theme

--- projects/pioneer-charts-web/src/main.ts
+++ projects/pioneer-charts-web/src/main.ts
@@ -1,5 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
-bootstrapApplication(AppComponent)
+bootstrapApplication(AppComponent, {
+  providers: [provideAnimationsAsync()]
+})
   .catch((err) => console.error(err));

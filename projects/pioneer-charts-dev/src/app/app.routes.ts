10f5317b4ce50cbb8612566c4628141866be53db
chad-ramos <chad@pioneercode.com>
2025-04-08 09:44:46 -0500

    chore: update routing to use loadComponent for lazy loading in app.routes.ts

--- projects/pioneer-charts-dev/src/app/app.routes.ts
+++ projects/pioneer-charts-dev/src/app/app.routes.ts
@@ -1,11 +1,11 @@
 import { Routes } from '@angular/router';
-import { ChartsComponent } from './charts/charts.component';
-import { TestComponent } from './test/test.component';
+
+
 
 
 
 export const routes: Routes = [
   { path: '', redirectTo: 'charts', pathMatch: 'full'},
-  { path: 'charts', component: ChartsComponent },
-  { path: 'test', component: TestComponent }
+  { path: 'charts', loadComponent: () => import('./charts/charts.component').then(m => m.ChartsComponent) },
+  { path: 'test', loadComponent: () => import('./test/test.component').then(m => m.TestComponent) }
 ];
--- projects/pioneer-charts-web/src/app/app.routes.ts
+++ projects/pioneer-charts-web/src/app/app.routes.ts
@@ -1,21 +1,21 @@
 import { Routes } from '@angular/router';
 
-import { HomeComponent } from './pages/home/home.component';
-import { IntroductionComponent } from './pages/docs/guides/introduction/introduction.component';
-import { ThemeComponent } from './pages/docs/guides/theme/theme.component';
-import { DataContractComponent } from './pages/docs/guides/data-contract/data-contract.component';
-import { ChartsComponent } from './pages/charts/charts.component';
-import { BarChartComponent } from './pages/docs/components/charts/bar-chart/bar-chart.component';
-import { LineAreaChartComponent } from './pages/docs/components/charts/line-area-chart/line-area-chart.component';
-import { PieChartComponent } from './pages/docs/components/charts/pie-chart/pie-chart.component';
+
+
+
+
+
+
+
+
 
 export const routes: Routes = [
-  { path: 'docs/guides/introduction', component: IntroductionComponent },
-  { path: 'docs/guides/data-contract', component: DataContractComponent },
-  { path: 'docs/guides/theme', component: ThemeComponent },
-  { path: 'docs/components/charts/bar-chart', component: BarChartComponent },
-  { path: 'docs/components/charts/line-area-chart', component: LineAreaChartComponent },
-  { path: 'docs/components/charts/pie-chart', component: PieChartComponent},
-  { path: 'charts', component: ChartsComponent },
-  { path: '**', component: HomeComponent }
+  { path: 'docs/guides/introduction', loadComponent: () => import('./pages/docs/guides/introduction/introduction.component').then(m => m.IntroductionComponent) },
+  { path: 'docs/guides/data-contract', loadComponent: () => import('./pages/docs/guides/data-contract/data-contract.component').then(m => m.DataContractComponent) },
+  { path: 'docs/guides/theme', loadComponent: () => import('./pages/docs/guides/theme/theme.component').then(m => m.ThemeComponent) },
+  { path: 'docs/components/charts/bar-chart', loadComponent: () => import('./pages/docs/components/charts/bar-chart/bar-chart.component').then(m => m.BarChartComponent) },
+  { path: 'docs/components/charts/line-area-chart', loadComponent: () => import('./pages/docs/components/charts/line-area-chart/line-area-chart.component').then(m => m.LineAreaChartComponent) },
+  { path: 'docs/components/charts/pie-chart', loadComponent: () => import('./pages/docs/components/charts/pie-chart/pie-chart.component').then(m => m.PieChartComponent)},
+  { path: 'charts', loadComponent: () => import('./pages/charts/charts.component').then(m => m.ChartsComponent) },
+  { path: '**', loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent) }
 ];

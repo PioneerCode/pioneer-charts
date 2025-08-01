a80d67e7bfe45b8c5de6b1f7e8d74d41f29090a6
chad-ramos <chad@pioneercode.com>
2025-08-01 12:41:12 -0500

    refactor: update Angular project configuration and dependencies, enhance app layout with toolbar, and remove obsolete test file

--- projects/pioneer-charts-docs/src/app/app.ts
+++ projects/pioneer-charts-docs/src/app/app.ts
@@ -1,9 +1,18 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule,
+  ],
   templateUrl: './app.html',
 })
 export class App {

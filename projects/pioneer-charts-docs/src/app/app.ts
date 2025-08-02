1781f4ab9700fa7c8633d165a0b5c032820f810a
chad-ramos <chad@pioneercode.com>
2025-08-02 12:07:44 -0500

    fix: correct provideZoneChangeDetection import and update app configuration style: remove unused Material imports and clean up index.html formatting feat: add initial app.scss file for styling

--- projects/pioneer-charts-docs/src/app/app.ts
+++ projects/pioneer-charts-docs/src/app/app.ts
@@ -1,19 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { MatToolbarModule } from '@angular/material/toolbar';
-
 
 @Component({
   selector: 'app-root',
-  imports: [
-    RouterOutlet,
-    MatIconModule,
-    MatButtonModule,
-    MatToolbarModule,
-  ],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
+  styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('pioneer-charts-docs');

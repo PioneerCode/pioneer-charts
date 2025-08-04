796c5c78e78d4f66911e611809fb6ba781117569
chad-ramos <chad@pioneercode.com>
2025-08-04 09:32:15 -0500

    refactor: remove unused Bootstrap styles and imports from SCSS files

--- projects/pioneer-charts-docs/src/app/app.config.ts
+++ projects/pioneer-charts-docs/src/app/app.config.ts
@@ -1,5 +1,6 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
@@ -8,6 +9,8 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideRouter(routes), 
+    provideClientHydration(withEventReplay()),
+    provideHttpClient()
   ]
 };

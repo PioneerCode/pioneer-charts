2840d6ed5b78e8f5dff8ade2313be4b252eaec63
chad-ramos <chad@pioneercode.com>
2025-08-12 10:35:23 -0500

    fix: update mock data paths in AppRepository to remove 'public' prefix

--- projects/pioneer-charts-docs/src/app/app.repository.ts
+++ projects/pioneer-charts-docs/src/app/app.repository.ts
@@ -20,60 +20,60 @@ export class AppRepository {
    * Bar Charts Horizontal
    */
   getBarHorizontalChart() {
-    return this.http.get<PcacBarHorizontalChartConfig>('./public/mock/bar-charts/bar-horizontal-chart.json');
+    return this.http.get<PcacBarHorizontalChartConfig>('./mock/bar-charts/bar-horizontal-chart.json');
   }
 
   getBarHorizontalChartSingle() {
-    return this.http.get<PcacBarHorizontalChartConfig>('./public/mock/bar-charts/bar-horizontal-chart-single.json');
+    return this.http.get<PcacBarHorizontalChartConfig>('./mock/bar-charts/bar-horizontal-chart-single.json');
   }
 
   getBarHorizontalChartGroup() {
-    return this.http.get<PcacBarHorizontalChartConfig>('./public/mock/bar-charts/bar-horizontal-chart-group.json');
+    return this.http.get<PcacBarHorizontalChartConfig>('./mock/bar-charts/bar-horizontal-chart-group.json');
   }
 
   getBarHorizontalChartStacked() {
-    return this.http.get<PcacBarHorizontalChartConfig>('./public/mock/bar-charts/bar-horizontal-chart-stacked.json');
+    return this.http.get<PcacBarHorizontalChartConfig>('./mock/bar-charts/bar-horizontal-chart-stacked.json');
   }
 
   /**
    * Bar Charts Vertical
    */
   getBarVerticalChart() {
-    return this.http.get<PcacBarVerticalChartConfig>('./public/mock/bar-charts/bar-vertical-chart.json');
+    return this.http.get<PcacBarVerticalChartConfig>('./mock/bar-charts/bar-vertical-chart.json');
   }
 
   getBarVerticalChartSingle() {
-    return this.http.get<PcacBarVerticalChartConfig>('./public/mock/bar-charts/bar-vertical-chart-single.json');
+    return this.http.get<PcacBarVerticalChartConfig>('./mock/bar-charts/bar-vertical-chart-single.json');
   }
 
   getBarVerticalChartGroup() {
-    return this.http.get<PcacBarVerticalChartConfig>('./public/mock/bar-charts/bar-vertical-chart-group.json');
+    return this.http.get<PcacBarVerticalChartConfig>('./mock/bar-charts/bar-vertical-chart-group.json');
   }
 
   getBarVerticalChartStacked() {
-    return this.http.get<PcacBarVerticalChartConfig>('./public/mock/bar-charts/bar-vertical-chart-stacked.json');
+    return this.http.get<PcacBarVerticalChartConfig>('./mock/bar-charts/bar-vertical-chart-stacked.json');
   }
 
   /**
    * Line Area Charts
    */
   getLineChart() {
-    return this.http.get<PcacLineAreaChartConfig>('./public/mock/line-area-chart/line-chart.json');
+    return this.http.get<PcacLineAreaChartConfig>('./mock/line-area-chart/line-chart.json');
   }
 
   getAreaChart() {
-    return this.http.get<PcacLineAreaChartConfig>('./public/mock/line-area-chart/area-chart.json');
+    return this.http.get<PcacLineAreaChartConfig>('./mock/line-area-chart/area-chart.json');
   }
 
   getAreaHideChart() {
-    return this.http.get<PcacLineAreaChartConfig>('./public/mock/line-area-chart/area-chart-hide.json');
+    return this.http.get<PcacLineAreaChartConfig>('./mock/line-area-chart/area-chart-hide.json');
   }
 
   getPieChartConfig() {
-    return this.http.get<PcacPieChartConfig>('./public/mock/pie-chart.json');
+    return this.http.get<PcacPieChartConfig>('./mock/pie-chart.json');
   }
 
   getShareConfig() {
-    return this.http.get<PcacData[]>('./public/mock/shared-config.json');
+    return this.http.get<PcacData[]>('./mock/shared-config.json');
   }
 }

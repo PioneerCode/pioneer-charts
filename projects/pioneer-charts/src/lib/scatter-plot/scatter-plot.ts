921b0c466ec3bc72c0b6bc893973b9d64a2f38b8
chad-ramos <chad@pioneercode.com>
2025-08-13 18:02:25 -0500

    feat: add scatter-plot component with initial template and class structure

---
+++ projects/pioneer-charts/src/lib/scatter-plot/scatter-plot.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-scatter-plot',
+  imports: [],
+  templateUrl: './scatter-plot.html'
+})
+export class PcacScatterPlot {
+
+}

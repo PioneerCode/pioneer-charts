54b8872ac545880e62f274f6911d908da0544317
chad-ramos <chad@pioneercode.com>
2025-08-13 17:01:25 -0500

    refactor: remove 404.js script and related build commands; fix typo in settings.json

--- build/404.js
+++
@@ -1,9 +0,0 @@
-import cpy from 'cpy';
-
-await cpy('./dist/pioneer-charts/index.html', './dist/pioneer-charts/', {
-  rename: () => `404.html`
-})
-
-console.log('Pioneer Charts: 404 copied!');
-
-

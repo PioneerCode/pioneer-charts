8056b4884f717da1e18e1fb6b4bb572a66c26e5b
chad-ramos <chad@pioneercode.com>
2025-08-14 17:41:59 -0500

    feat: add theme documentation and routing, enhance navigation links

--- projects/pioneer-charts-docs/src/app/app.routes.ts
+++ projects/pioneer-charts-docs/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     { path: 'docs/guides/introduction', loadComponent: () => import('./docs/introduction/introduction.component').then(m => m.IntroductionComponent) },
+    { path: 'docs/guides/theme', loadComponent: () => import('./docs/theme/theme.component').then(m => m.ThemeComponent) },
     { path: 'charts', loadComponent: () => import('./charts/charts.component').then(m => m.ChartsComponent) },
     { path: '**', loadComponent: () => import('./home/home').then(m => m.Home) }
 ];
--- projects/pioneer-charts-docs/src/app/layout/page-docs/page-docs.ts
+++ projects/pioneer-charts-docs/src/app/layout/page-docs/page-docs.ts
@@ -2,9 +2,11 @@ import { Component, inject, input, signal } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MediaMatcher } from '@angular/cdk/layout';
 import { MatListModule } from '@angular/material/list';
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-layout-page-docs',
   imports: [
+    RouterLink,
     MatSidenavModule,
     MatListModule
   ],

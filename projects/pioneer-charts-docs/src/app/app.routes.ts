c149f957b2d0658f60998231124308121acdf55d
chad-ramos <chad@pioneercode.com>
2025-08-14 18:13:40 -0500

    feat: add bar chart component with configuration and API documentation

--- projects/pioneer-charts-docs/src/app/app.routes.ts
+++ projects/pioneer-charts-docs/src/app/app.routes.ts
@@ -4,6 +4,7 @@ export const routes: Routes = [
     { path: 'docs/guides/introduction', loadComponent: () => import('./docs/introduction/introduction.component').then(m => m.IntroductionComponent) },
     { path: 'docs/guides/theme', loadComponent: () => import('./docs/theme/theme.component').then(m => m.ThemeComponent) },
     { path: 'docs/guides/data-contract', loadComponent: () => import('./docs/data-contract/data-contract.component').then(m => m.DataContractComponent) },
+    { path: 'docs/components/charts/bar-chart', loadComponent: () => import('./docs/bar-chart/bar-chart.component').then(m => m.BarChartComponent) },
     { path: 'charts', loadComponent: () => import('./charts/charts.component').then(m => m.ChartsComponent) },
     { path: '**', loadComponent: () => import('./home/home').then(m => m.Home) }
 ];
---
+++ projects/pioneer-charts-docs/src/app/docs/bar-chart/bar-chart.component.ts
@@ -0,0 +1,75 @@
+import { Component, inject } from '@angular/core';
+import { PcacBarHorizontalChartComponent, PcacBarVerticalChartComponent } from '@pioneer-code/pioneer-charts';
+import { MatCardModule } from '@angular/material/card';
+
+import { LayoutCode } from '../../layout/code/code';
+import { LayoutPageDocs } from '../../layout/page-docs/page-docs';
+import { AppService } from '../../app.service';
+
+
+@Component({
+  selector: 'pc-bar-chart',
+  templateUrl: './bar-chart.component.html',
+  imports: [
+    LayoutCode,
+    LayoutPageDocs,
+    MatCardModule,
+    // PageHeaderComponent,
+    // DocLayoutComponent,
+    // BaseConfigComponent,
+    // CodeComponent,
+    // StringifyPipe,
+    PcacBarHorizontalChartComponent,
+    PcacBarVerticalChartComponent
+  ]
+})
+export class BarChartComponent {
+  verticalCode = `<pcac-bar-vertical-chart [config]="barVerticalChartConfig" (barClicked)="onClicked($event)"></pcac-bar-vertical-chart>`;
+  horizontalCode = `<pcac-bar-horizontal-chart [config]="barHorizontalChartConfig" (barClicked)="onClicked($event)"></pcac-bar-horizontal-chart>`;
+  importCode = `import { PcacBarVerticalChartModule, PcacBarHorizontalChartModule } from '@pioneer-code/pioneer-charts';`;
+  // jumpNav = [
+  //   {
+  //     key: 'Bar Chart',
+  //     value: 'bar-chart',
+  //     level: JumpNavLevel.h1
+  //   },
+  //   {
+  //     key: 'Markup',
+  //     value: 'markup',
+  //     level: JumpNavLevel.h2
+  //   },
+  //   {
+  //     key: 'Horizontal',
+  //     value: 'horizontal',
+  //     level: JumpNavLevel.h3
+  //   },
+  //   {
+  //     key: 'Vertical',
+  //     value: 'vertical',
+  //     level: JumpNavLevel.h3
+  //   },
+  //   {
+  //     key: 'API',
+  //     value: 'api',
+  //     level: JumpNavLevel.h2
+
+  //   },
+  //   {
+  //     key: 'Configuration',
+  //     value: 'configuration',
+  //     level: JumpNavLevel.h3
+  //   },
+  //   {
+  //     key: 'Events',
+  //     value: 'events',
+  //     level: JumpNavLevel.h2
+  //   },
+  //   {
+  //     key: 'Contract',
+  //     value: 'contract',
+  //     level: JumpNavLevel.h2
+  //   }
+  // ] as IJumpNav[];
+
+  readonly service = inject(AppService);
+}

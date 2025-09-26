c930dae3613180c7c526f53cd17b64856cdaa3b8
chad-ramos <chad@pioneercode.com>
2025-09-26 15:54:21 -0500

    refactor: enhance navigation links with active state and update accent color opacity

--- projects/pioneer-charts-docs/src/app/layout/page-docs/navigation/navigation.ts
+++ projects/pioneer-charts-docs/src/app/layout/page-docs/navigation/navigation.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { MatListModule } from '@angular/material/list';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-layout-page-docs-navigation',
   imports: [
     RouterLink,
+    RouterLinkActive,
     MatListModule
   ],
   templateUrl: './navigation.html',

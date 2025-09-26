9a3f5cb25e2f1d2a07e87dc64bc32daf1d6f97ef
chad-ramos <chad@pioneercode.com>
2025-09-26 15:41:11 -0500

    refactor: update introduction documentation for component imports and remove unused module examples

--- projects/pioneer-charts-docs/src/app/docs/introduction/introduction.component.ts
+++ projects/pioneer-charts-docs/src/app/docs/introduction/introduction.component.ts
@@ -42,28 +42,11 @@ export class IntroductionComponent {
 
   npmInstallCode = `npm install --save @pioneer-code/pioneer-charts`;
   yarnInstallCode = `yarn add @pioneer-code/pioneer-charts`;
-  appModuleCode = `import {PcacTableModule} from '@pioneer-code/pioneer-charts';
+  appModuleCode = `import {PcacLineChart} from '@pioneer-code/pioneer-charts';
 
 @NgModule({
   ...
-  imports: [PcacTableModule],
-  ...
-})
-export class YourAppModule { }`;
-
-  specialModuleCode = `import {PcacTableModule, PcacLineAreaChartModule} from '@pioneer-code/pioneer-charts';
-
-@NgModule({
-  imports: [PcacTableModule, PcacLineAreaChartModule],
-  exports: [PcacTableModule, PcacLineAreaChartModule],
-})
-export class PioneerChartsImportModule { }`;
-
-  appSpecialModuleCode = `import {PioneerChartsImportModule} from './pioneer-charts-import.module;
-
-@NgModule({
-  ...
-  imports: [PioneerChartsImportModule],
+  imports: [PcacLineChart],
   ...
 })
 export class YourAppModule { }`;
